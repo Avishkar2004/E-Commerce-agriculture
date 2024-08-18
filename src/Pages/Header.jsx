e9e54a70aa133355380cd6c70a8cc71cedbbb2db
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-08-18 17:58:54 +0530

    header

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -89,7 +89,7 @@ const Header = () => {
               Profile
             </Link>
             <button
-              onClick={ProfilehandleLogOut}
+              onClick={handleLogOut}
               className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100 transition-colors duration-200"
             >
               Logout
